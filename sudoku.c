98556f12e31822f54d6c2e73ec92f77631d97c20
JORGEVILLARREAL <>
2022-05-12 15:53:08 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,7 +45,7 @@ void print_node(Node* n){
 
 int is_valid(Node* n)
 {
-  int i;
+  int i, j;
   int *ptr = (int*) calloc(10, sizeof(int));
   
   for(i = 0; i < 9; i++)
@@ -55,6 +55,21 @@ int is_valid(Node* n)
       ptr[i] = ptr[i] + 1;
       if(ptr[i] > 1) return 0;
     }
+  }
+  for(j = 0; j < 9; j++)
+  {
+    if(n->sudo[0][j] != 0)
+    {
+      ptr[j] = ptr[j] + 1;
+      if(ptr[j] > 1) return 0;
+    }
+  }
+  int k=4,p; 
+  for(p=0;p<9;p++){
+  int i=3*(k/3) + (p/3) ;
+  int j=3*(k%3) + (p%3) ;
+  printf("%d ",n->sudo[i][j]);
+  if(p%3 == 2) printf("\n");
   }
   return 1;
 }
